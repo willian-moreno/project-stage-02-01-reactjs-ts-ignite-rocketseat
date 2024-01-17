10f3931c5de1e7f32fe3cc23c1a4d5fdc5068de6
willianam <willianam@uninove.br>
2024-01-17 13:00:49 -0300

    refactor: Remoção styled componentes não utilizados

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -17,38 +17,6 @@ export const HomeContainer = styled.main`
   }
 `
 
-export const CountdownContainer = styled.div`
-  display: flex;
-  gap: 1.6rem;
-
-  font-family: 'Roboto Mono', monospace, sans-serif;
-  font-size: 16rem;
-  line-height: 12.8rem;
-
-  color: ${(props) => props.theme['gray-100']};
-
-  span {
-    background-color: ${(props) => props.theme['gray-700']};
-
-    padding: 3.2rem 1.6rem;
-
-    border-radius: 8px;
-  }
-`
-
-export const Separator = styled.div`
-  display: flex;
-  justify-content: center;
-
-  width: 6.4rem;
-
-  padding: 3.2rem 0;
-
-  color: ${(props) => props.theme['green-500']};
-
-  overflow: hidden;
-`
-
 const btnConfig = {
   success: {
     bgColor: 'green-500',
